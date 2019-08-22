6f840294dd3038cea095fab2dbd08fa3d2f209a5
Ovidiu Stoica <ovytkl@gmail.com>
2019-08-22 20:48:57 +0300

    Added navigation with react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+
+const HatsPage = () => (
+	<div>
+		<h1>HATS</h1>
+	</div>
+);
 
 function App() {
-  return (
-    <div>
-      <Homepage />
-    </div>
-  );
+	return (
+		<div>
+			<Switch>
+				<Route exact path='/' component={Homepage} />
+				<Route path='/hats' component={HatsPage} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -12,28 +12,33 @@ export default class Directory extends React.Component {
 					title: 'hats',
 					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
 					id: 1,
+					linkUrl: 'hats',
 				},
 				{
 					title: 'jackets',
 					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
 					id: 2,
+					linkUrl: 'jackets',
 				},
 				{
 					title: 'sneakers',
 					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
 					id: 3,
+					linkUrl: 'sneakers',
 				},
 				{
 					title: 'womens',
 					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
 					size: 'large',
 					id: 4,
+					linkUrl: 'womens',
 				},
 				{
 					title: 'mens',
 					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
 					size: 'large',
 					id: 5,
+					linkUrl: 'mens',
 				},
 			],
 		};
@@ -42,8 +47,8 @@ export default class Directory extends React.Component {
 	render() {
 		return (
 			<div className='directory'>
-				{this.state.sections.map(({ title, imageUrl, size, id }) => (
-					<MenuItem title={title} imageUrl={imageUrl} size={size} key={id} />
+				{this.state.sections.map(({ id, ...sectionProps }) => (
+					<MenuItem key={id} {...sectionProps} />
 				))}
 			</div>
 		);

98a358ecacb31326972109dda6bf3a4396a159d6
Ovidiu Stoica <ovytkl@gmail.com>
2019-08-22 08:25:18 +0300

    finished menu categories items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,22 +10,29 @@ export default class Directory extends React.Component {
 			sections: [
 				{
 					title: 'hats',
+					imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
 					id: 1,
 				},
 				{
 					title: 'jackets',
+					imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
 					id: 2,
 				},
 				{
 					title: 'sneakers',
+					imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
 					id: 3,
 				},
 				{
 					title: 'womens',
+					imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+					size: 'large',
 					id: 4,
 				},
 				{
 					title: 'mens',
+					imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+					size: 'large',
 					id: 5,
 				},
 			],
@@ -35,8 +42,8 @@ export default class Directory extends React.Component {
 	render() {
 		return (
 			<div className='directory'>
-				{this.state.sections.map(({ title, id }) => (
-					<MenuItem title={title} key={id} />
+				{this.state.sections.map(({ title, imageUrl, size, id }) => (
+					<MenuItem title={title} imageUrl={imageUrl} size={size} key={id} />
 				))}
 			</div>
 		);

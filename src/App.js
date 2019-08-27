c2ce97c09fdd1772aa5be4248eb0d099d134430f
Ovidiu Stoica <ovytkl@gmail.com>
2019-08-27 06:37:13 +0300

    Added collection preview and a new shop page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
-const HatsPage = () => (
-	<div>
-		<h1>HATS</h1>
-	</div>
-);
-
 function App() {
-	return (
-		<div>
-			<Switch>
-				<Route exact path='/' component={Homepage} />
-				<Route path='/hats' component={HatsPage} />
-			</Switch>
-		</div>
-	);
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;

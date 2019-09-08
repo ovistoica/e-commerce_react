44c9803766c3079bd8cc3d4c1205b7fbb66d3890
Ovidiu Stoica <ovytkl@gmail.com>
2019-09-08 13:24:48 +0300

    Items are now added to the cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
 import './cart-dropdown.styles.scss';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropDown = () => (
+const CartDropDown = ({ cartItems }) => (
   <div className='cart-dropdown'>
     <div className='cart-icons'>
-      <div>ITEM</div>
-      <div>ITEM</div>
-      <div>ITEM</div>
-      <div>ITEM</div>
-      <div>ITEM</div>
-      <div>ITEM</div>
-      <div>ITEM</div>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
     </div>
     <CustomButton> GO TO CHECKOUT </CustomButton>
   </div>
 );
 
-export default CartDropDown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropDown);

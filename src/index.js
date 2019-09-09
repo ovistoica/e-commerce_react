c9208aab6cd2b876e682aeac516e4829f6ca60ca
Ovidiu Stoica <ovytkl@gmail.com>
2019-09-09 07:45:07 +0300

    refactor styles to fit better on screen

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
 import store from './redux/store/store';
 
+import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <Provider store={store}>
